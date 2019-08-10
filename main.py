012175dfda97eceebd2cd593e8edd3c959d44592
Haoyuku <harrison.h.y.ku@gmail.com>
2019-08-10 15:10:49 +0800

    resnet

--- main.py
+++ main.py
@@ -1,31 +1,24 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 import os
-import time
-import json
-from collections import OrderedDict
-import importlib
-import logging
+import sys
 import argparse
-import numpy as np
+import json
+import time
 import random
+import logging
+import numpy as np
+from tqdm import tqdm
+from collections import OrderedDict
 
 import torch
 import torch.nn as nn
 import torch.optim
 import torch.utils.data
-import torch.backends.cudnn
+import torch.backends.cuda
 import torchvision.utils
-try:
-    from tensorboardX import SummaryWriter
-    is_tensorboard_available = True
-except Exception:
-    is_tensorboard_available = False
 
-from dataloader import get_loader
+from models import GazeNet
+from preprocess import MPIIFaceGaze
 
-torch.backends.cudnn.benchmark = True
 
 logging.basicConfig(
     format='[%(asctime)s %(name)s %(levelname)s] - %(message)s',
@@ -34,55 +27,6 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 global_step = 0
-
-
-def str2bool(s):
-    if s.lower() == 'true':
-        return True
-    elif s.lower() == 'false':
-        return False
-    else:
-        raise RuntimeError('Boolean value expected')
-
-
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '--arch', type=str, required=True, choices=['lenet', 'resnet_preact'])
-    parser.add_argument('--dataset', type=str, required=True)
-    parser.add_argument('--test_id', type=int, required=True)
-    parser.add_argument('--outdir', type=str, required=True)
-    parser.add_argument('--seed', type=int, default=17)
-    parser.add_argument('--num_workers', type=int, default=7)
-
-    # optimizer
-    parser.add_argument('--epochs', type=int, default=40)
-    parser.add_argument('--batch_size', type=int, default=64)
-    parser.add_argument('--base_lr', type=float, default=0.01)
-    parser.add_argument('--weight_decay', type=float, default=1e-4)
-    parser.add_argument('--momentum', type=float, default=0.9)
-    parser.add_argument('--nesterov', type=str2bool, default=True)
-    parser.add_argument('--milestones', type=str, default='[20, 30]')
-    parser.add_argument('--lr_decay', type=float, default=0.1)
-
-    # TensorBoard
-    parser.add_argument(
-        '--tensorboard', dest='tensorboard', action='store_true', default=True)
-    parser.add_argument(
-        '--no-tensorboard', dest='tensorboard', action='store_false')
-    parser.add_argument('--tensorboard_images', action='store_true')
-    parser.add_argument('--tensorboard_parameters', action='store_true')
-
-    args = parser.parse_args()
-    if not is_tensorboard_available:
-        args.tensorboard = False
-        args.tensorboard_images = False
-        args.tensorboard_parameters = False
-
-    assert os.path.exists(args.dataset)
-    args.milestones = json.loads(args.milestones)
-
-    return args
 
 
 class AverageMeter(object):
@@ -130,21 +74,23 @@ def train(epoch, model, optimizer, criterion, train_loader, config, writer):
     loss_meter = AverageMeter()
     angle_error_meter = AverageMeter()
     start = time.time()
-    for step, (images, poses, gazes) in enumerate(train_loader):
+    for step, (images, gazes, headposes) in enumerate(train_loader):
         global_step += 1
 
-        if config['tensorboard_images'] and step == 0:
-            image = torchvision.utils.make_grid(
-                images, normalize=True, scale_each=True)
-            writer.add_image('Train/Image', image, epoch)
+        # if config['tensorboard_images'] and step == 0:
+        #     image = torchvision.utils.make_grid(
+        #         images, normalize=True, scale_each=True)
+        #     writer.add_image('Train/Image', image, epoch)
 
         images = images.cuda()
-        poses = poses.cuda()
         gazes = gazes.cuda()
 
         optimizer.zero_grad()
 
-        outputs = model(images, poses)
+        outputs = model(images)
+
+        #print(outputs)
+
         loss = criterion(outputs, gazes)
         loss.backward()
 
@@ -156,29 +102,39 @@ def train(epoch, model, optimizer, criterion, train_loader, config, writer):
         loss_meter.update(loss.item(), num)
         angle_error_meter.update(angle_error.item(), num)
 
-        if config['tensorboard']:
-            writer.add_scalar('Train/RunningLoss', loss_meter.val, global_step)
+        # if config['tensorboard']:
+        #     writer.add_scalar('Train/RunningLoss', loss_meter.val, global_step)
 
-        if step % 100 == 0:
-            logger.info('Epoch {} Step {}/{} '
-                        'Loss {:.4f} ({:.4f}) '
-                        'AngleError {:.2f} ({:.2f})'.format(
-                            epoch,
-                            step,
-                            len(train_loader),
-                            loss_meter.val,
-                            loss_meter.avg,
-                            angle_error_meter.val,
-                            angle_error_meter.avg,
-                        ))
+        # if step % 1000 == 0:
+        #     logger.info('Epoch {} Step {}/{} '
+        #                 'Loss {:.4f} ({:.4f}) '
+        #                 'AngleError {:.2f} ({:.2f})'.format(
+        #                     epoch,
+        #                     step,
+        #                     len(train_loader),
+        #                     loss_meter.val,
+        #                     loss_meter.avg,
+        #                     angle_error_meter.val,
+        #                     angle_error_meter.avg,
+        #                 ))
+
+    logger.info('Epoch {} '
+                'Loss {:.4f} ({:.4f}) '
+                'AngleError {:.2f} ({:.2f})'.format(
+                    epoch,
+                    loss_meter.val,
+                    loss_meter.avg,
+                    angle_error_meter.val,
+                    angle_error_meter.avg,
+                ))
 
     elapsed = time.time() - start
     logger.info('Elapsed {:.2f}'.format(elapsed))
 
-    if config['tensorboard']:
-        writer.add_scalar('Train/Loss', loss_meter.avg, epoch)
-        writer.add_scalar('Train/AngleError', angle_error_meter.avg, epoch)
-        writer.add_scalar('Train/Time', elapsed, epoch)
+    # if config['tensorboard']:
+    #     writer.add_scalar('Train/Loss', loss_meter.avg, epoch)
+    #     writer.add_scalar('Train/AngleError', angle_error_meter.avg, epoch)
+    #     writer.add_scalar('Train/Time', elapsed, epoch)
 
 
 def test(epoch, model, criterion, test_loader, config, writer):
@@ -189,18 +145,17 @@ def test(epoch, model, criterion, test_loader, config, writer):
     loss_meter = AverageMeter()
     angle_error_meter = AverageMeter()
     start = time.time()
-    for step, (images, poses, gazes) in enumerate(test_loader):
-        if config['tensorboard_images'] and epoch == 0 and step == 0:
-            image = torchvision.utils.make_grid(
-                images, normalize=True, scale_each=True)
-            writer.add_image('Test/Image', image, epoch)
+    for step, (images, gazes, headposes) in enumerate(test_loader):
+        # if config['tensorboard_images'] and epoch == 0 and step == 0:
+        #     image = torchvision.utils.make_grid(
+        #         images, normalize=True, scale_each=True)
+        #     writer.add_image('Test/Image', image, epoch)
 
         images = images.cuda()
-        poses = poses.cuda()
         gazes = gazes.cuda()
 
         with torch.no_grad():
-            outputs = model(images, poses)
+            outputs = model(images)
         loss = criterion(outputs, gazes)
 
         angle_error = compute_angle_error(outputs, gazes).mean()
@@ -215,51 +170,70 @@ def test(epoch, model, criterion, test_loader, config, writer):
     elapsed = time.time() - start
     logger.info('Elapsed {:.2f}'.format(elapsed))
 
-    if config['tensorboard']:
-        if epoch > 0:
-            writer.add_scalar('Test/Loss', loss_meter.avg, epoch)
-            writer.add_scalar('Test/AngleError', angle_error_meter.avg, epoch)
-        writer.add_scalar('Test/Time', elapsed, epoch)
+    # if config['tensorboard']:
+    #     if epoch > 0:
+    #         writer.add_scalar('Test/Loss', loss_meter.avg, epoch)
+    #         writer.add_scalar('Test/AngleError', angle_error_meter.avg, epoch)
+    #     writer.add_scalar('Test/Time', elapsed, epoch)
 
-    if config['tensorboard_parameters']:
-        for name, param in model.named_parameters():
-            writer.add_histogram(name, param, global_step)
+    # if config['tensorboard_parameters']:
+    #     for name, param in model.named_parameters():
+    #         writer.add_histogram(name, param, global_step)
 
     return angle_error_meter.avg
 
 
-def main():
-    args = parse_args()
+def main(args):
     logger.info(json.dumps(vars(args), indent=2))
 
     # TensorBoard SummaryWriter
-    writer = SummaryWriter() if args.tensorboard else None
+    # writer = SummaryWriter() if args.tensorboard else None
+    writer = None
 
     # set random seed
-    seed = args.seed
-    torch.manual_seed(seed)
-    np.random.seed(seed)
+    seed = 999
     random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
 
     # create output directory
     outdir = args.outdir
     if not os.path.exists(outdir):
         os.makedirs(outdir)
 
-    outpath = os.path.join(outdir, 'config.json')
-    with open(outpath, 'w') as fout:
-        json.dump(vars(args), fout, indent=2)
+    # outpath = os.path.join(outdir, 'config.json')
+    # with open(outpath, 'w') as fout:
+    #     json.dump(vars(args), fout, indent=2)
 
     # data loaders
-    train_loader, test_loader = get_loader(
-        args.dataset, args.test_id, args.batch_size, args.num_workers, True)
+    train_set = MPIIFaceGaze(range(0,14), args.images)
+    test_set = MPIIFaceGaze(range(14,15), args.images)
+
+    batch_size = 32
+    train_loader = torch.utils.data.DataLoader(
+        train_set, 
+        batch_size = batch_size, 
+        shuffle=True, 
+        num_workers=args.num_workers,
+        pin_memory=True,
+        drop_last=True,
+    )
+    test_loader = torch.utils.data.DataLoader(
+        test_set, 
+        batch_size = batch_size, 
+        shuffle=False, 
+        num_workers=args.num_workers,
+        pin_memory=True,
+        drop_last=False,
+    )
+
+    # train_loader, test_loader = get_loader(
+    #     args.dataset, args.test_id, args.batch_size, args.num_workers, True)
 
     # model
-    module = importlib.import_module('models.{}'.format(args.arch))
-    model = module.Model()
-    model.cuda()
-
-    criterion = nn.MSELoss(size_average=True)
+    device = 'cuda'
+    model = GazeNet().to(device)
+    criterion = nn.MSELoss(size_average=True).to(device)
 
     # optimizer
     optimizer = torch.optim.SGD(
@@ -268,22 +242,20 @@ def main():
         momentum=args.momentum,
         weight_decay=args.weight_decay,
         nesterov=args.nesterov)
+    
     scheduler = torch.optim.lr_scheduler.MultiStepLR(
-        optimizer, milestones=args.milestones, gamma=args.lr_decay)
+        optimizer, milestones=[20, 30], gamma=args.lr_decay)
 
-    config = {
-        'tensorboard': args.tensorboard,
-        'tensorboard_images': args.tensorboard_images,
-        'tensorboard_parameters': args.tensorboard_parameters,
-    }
+    config = None
 
     # run test before start training
-    test(0, model, criterion, test_loader, config, writer)
+    #test(0, model, criterion, test_loader, config, writer)
 
     for epoch in range(1, args.epochs + 1):
         scheduler.step()
 
         train(epoch, model, optimizer, criterion, train_loader, config, writer)
+        
         angle_error = test(epoch, model, criterion, test_loader, config,
                            writer)
 
@@ -294,13 +266,34 @@ def main():
             ('epoch', epoch),
             ('angle_error', angle_error),
         ])
-        model_path = os.path.join(outdir, 'model_state.pth')
-        torch.save(state, model_path)
+        #model_path = os.path.join(outdir, 'model_state.pth')
+        torch.save(state, 'model_state.pth')
 
-    if args.tensorboard:
-        outpath = os.path.join(outdir, 'all_scalars.json')
-        writer.export_scalars_to_json(outpath)
+    # if args.tensorboard:
+    #     outpath = os.path.join(outdir, 'all_scalars.json')
+    #     writer.export_scalars_to_json(outpath)
+
+
+def parse_arguments(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', type=str, default='/dataset/MPIIFaceGaze_normalized')
+    parser.add_argument('--images', type=str, default='./mpii_data/img/')
+    parser.add_argument('--test_id', type=int, default=0)
+    parser.add_argument('--outdir', type=str, default='./')
+    parser.add_argument('--seed', type=int, default=6)
+    parser.add_argument('--num_workers', type=int, default=8)
+
+    # optimizer
+    parser.add_argument('--epochs', type=int, default=10)
+    parser.add_argument('--base_lr', type=float, default=0.01)
+    parser.add_argument('--weight_decay', type=float, default=1e-4)
+    parser.add_argument('--momentum', type=float, default=0.9)
+    parser.add_argument('--nesterov', type=bool, default=True)
+    parser.add_argument('--milestones', type=str, default='[20, 30]')
+    parser.add_argument('--lr_decay', type=float, default=0.1)
+
+    return parser.parse_args(argv)
 
 
 if __name__ == '__main__':
-    main()
+    main(parse_arguments(sys.argv[1:]))
--- models.py
+++ models.py
@@ -8,45 +8,55 @@ class GazeNet(nn.Module):
 
     def __init__(self):
         super(GazeNet, self).__init__()
-        model = models.alexnet(pretrained=True)
-        self.alexnet = model.features
+        res = models.resnet18(pretrained=True)
 
-        self.Conv1 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=1, stride=1, padding=0)
-        self.Conv2 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=1, stride=1, padding=0)
-        self.Conv3 = nn.Conv2d(in_channels=256, out_channels=1, kernel_size=1, stride=1, padding=0)
+        self.features = nn.Sequential(
+            res.conv1,
+            res.bn1,
+            res.relu,
+            res.maxpool,
+            res.layer1,
+            res.layer2,
+            res.layer3,
+            res.layer4
+        )
+
+        # self.Conv1 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=1, stride=1, padding=0)
+        # self.Conv2 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=1, stride=1, padding=0)
+        # self.Conv3 = nn.Conv2d(in_channels=512, out_channels=1, kernel_size=1, stride=1, padding=0)
         self.layer1 = nn.Sequential(
-            nn.Linear(256*7*7, 4096),
+            nn.Linear(512*8*8, 4096),
             nn.ReLU(),
             nn.Dropout(0.5)
         )
         self.layer2 = nn.Sequential(
-            nn.Linear(4096, 4096),
+            nn.Linear(4096, 512),
             nn.ReLU(),
             nn.Dropout(0.5)
         )   
-        self.fc = nn.Linear(4096, 2)
+        self.fc = nn.Linear(512, 2)
 
-        self._initialize_weight()
-        self._initialize_bias()
+    #     self._initialize_weight()
+    #     self._initialize_bias()
 
-    def _initialize_weight(self):
-        nn.init.normal_(self.Conv1.weight, mean=0.0, std=0.01)
-        nn.init.normal_(self.Conv2.weight, mean=0.0, std=0.01)
-        nn.init.normal_(self.Conv3.weight, mean=0.0, std=0.001)
+    # def _initialize_weight(self):
+    #     nn.init.normal_(self.Conv1.weight, mean=0.0, std=0.01)
+    #     nn.init.normal_(self.Conv2.weight, mean=0.0, std=0.01)
+    #     nn.init.normal_(self.Conv3.weight, mean=0.0, std=0.001)
 
-    def _initialize_bias(self):
-        nn.init.constant_(self.Conv1.bias, val=0.1)
-        nn.init.constant_(self.Conv2.bias, val=0.1)
-        nn.init.constant_(self.Conv3.bias, val=1)
+    # def _initialize_bias(self):
+    #     nn.init.constant_(self.Conv1.bias, val=0.1)
+    #     nn.init.constant_(self.Conv2.bias, val=0.1)
+    #     nn.init.constant_(self.Conv3.bias, val=1)
 
-    def forward(self, x, z):
-        x = self.alexnet(x)
+    def forward(self, x):
+        x = self.features(x)
         
-        y = F.relu(self.Conv1(x))
-        y = F.relu(self.Conv2(y))
-        y = F.relu(self.Conv3(y))
+        # y = F.relu(self.Conv1(x))
+        # y = F.relu(self.Conv2(y))
+        # y = F.relu(self.Conv3(y))
 
-        x = F.dropout(F.relu(torch.mul(x, y)), 0.5)
+        # x = F.dropout(F.relu(torch.mul(x, y)), 0.5)
         x = x.view(x.size(0), -1)
         x = self.layer1(x)
         x = self.layer2(x)
